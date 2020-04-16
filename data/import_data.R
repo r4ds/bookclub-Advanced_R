1dc6b0e4433b24e393c062b8d30955dbdb92902a
Maya <jaffe.maya@gmail.com>
2020-04-16 12:57:14 -0700

    removing files

--- data/import_data.R
+++ data/import_data.R
@@ -1,9 +1,8 @@
 # data from tidytuesday
 # https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-03-31/readme.md
 
-brewing_materials <- readr::read_csv('data/brewing_materials.csv')
-beer_taxed <- readr::read_csv('data/beer_taxed.csv')
-brewer_size <- readr::read_csv('data/brewer_size.csv')
-beer_states <- readr::read_csv('data/beer_states.csv')
-
+brewing_materials <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/brewing_materials.csv')
+beer_taxed <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/beer_taxed.csv')
+brewer_size <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/brewer_size.csv')
+beer_states <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/beer_states.csv')
 
